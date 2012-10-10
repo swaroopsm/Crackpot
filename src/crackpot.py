ce420967b7c3c83a21f2be5dabdf9dbe2f14f2eb
Swaroop SM <swaroop.striker@gmail.com>
2012-10-10 14:46:16 +0530

    initial setup and created index route

--- src/crackpot.py
+++ src/crackpot.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
 
+from flask import Flask
 
+app=Flask("Crackpot")
+
+@app.route("/")
+def index():
+	return "Welcome Crackster!"
+	
+if __name__=="__main__":
+	app.debug=True
+	app.run()
