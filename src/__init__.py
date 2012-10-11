1b0aef0c7e0926c88e0c8bb0b0ea049af3e9b494
Swaroop SM <swaroop.striker@gmail.com>
2012-10-11 22:58:13 +0530

    render home.html template as the index page

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask("Crackpot")
 
 @app.route("/")
 def index():
-	return "Welcome Crackster!"
+	return render_template("home.html",title="Crackpot | Welcome Crackster!")
 	
 if __name__=="__main__":
 	app.debug=True
