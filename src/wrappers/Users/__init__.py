019e9fd25d2dd4297a4e0b7bf5831eb69c8e3326
Swaroop SM <swaroop.striker@gmail.com>
2012-10-13 23:59:11 +0530

    defined new function that adds a new user

--- src/wrappers/Users/__init__.py
+++ src/wrappers/Users/__init__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
-import sha
+import sha, json
 import settings as s
 
 class Users:
 	
 	@staticmethod
-	def new():
-		return "Hello"
+	def new(m,name,email,password,username):
+		salt=s.PASSWORD_SALT
+		password=sha.new(salt).hexdigest()+sha.new(password).hexdigest()
+		try:
+			st=str(m.db.users.save({'name':name,'email':email,'password':password,'username':username}))
+			return json.dumps({'_id':st,'status': 'success'})
+		except:
+			return json.dumps({'status': 'error'})
