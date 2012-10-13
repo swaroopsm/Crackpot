c52b038dc70978f4dd367d277c0f8aea54a90561
Swaroop SM <swaroop.striker@gmail.com>
2012-10-13 21:34:26 +0530

    imported PyMongo module and settings file and also created a PyMongo object

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
 from flask import Flask, render_template
+from flask.ext.pymongo import PyMongo
+import settings as s
 
 app=Flask("Crackpot")
+
+app.config['MONGO_HOST']=s.MONGO_HOST
+app.config['MONGO_PORT']=s.MONGO_PORT
+app.config['MONGO_USERNAME']=s.MONGO_USERNAME
+app.config['MONGO_PASSWORD']=s.MONGO_PASSWORD
+app.config['MONGO_DBNAME']=s.MONGO_DBNAME
+
+mongo=PyMongo(app)
 
 @app.route("/")
 def index():
@@ -11,7 +21,7 @@ def index():
 @app.route("/signup")
 def signup():
 	return render_template("signup.html", title="Crackpot | New Registration")
-	
+
 if __name__=="__main__":
 	app.debug=True
 	app.run()
