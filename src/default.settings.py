dbf14eb7e1ddd6d2c3bd9472189763cc3d6d985a
Swaroop SM <swaroop.striker@gmail.com>
2012-10-13 21:33:09 +0530

    added default config settings for MongoDB

---
+++ src/default.settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+##																		 ##
+## CHANGE THIS FILENAME TO settings.py ##
+##																		 ##
+
+MONGO_HOST     = "YOUR_SERVER_NAME"
+MONGO_PORT     =  YOUR_PORT
+MONGO_USERNAME = "YOUR_SERVER_USER_NAME"
+MONGO_PASSWORD = "YOUR_SERVER_PASSWORD"
+MONGO_DBNAME   = "YOUR_DB_NAME"
