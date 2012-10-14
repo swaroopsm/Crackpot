1fb42fa1dff91aec86c59cb9a4d6386f3c62c322
Swaroop SM <swaroop.striker@gmail.com>
2012-10-14 13:12:25 +0530

    defined login method

--- src/wrappers/Sessions/__init__.py
+++ src/wrappers/Sessions/__init__.py
@@ -6,5 +6,7 @@ import settings as s
 class Sessions:
 	
 	@staticmethod
-	def login(username,password):
-		return False
+	def login(m,username,password):
+		password=sha.new(s.PASSWORD_SALT).hexdigest()+sha.new(password).hexdigest()
+		st=m.db.users.find({"username": username,"password": password}).count()
+		return st
