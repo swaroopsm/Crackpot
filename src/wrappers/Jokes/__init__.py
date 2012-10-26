056d8db4aa3f8664d1b39e1568981c0d352f9471
Swaroop SM <swaroop.striker@gmail.com>
2012-10-27 01:16:21 +0530

    insert joke date into mongo

--- src/wrappers/Jokes/__init__.py
+++ src/wrappers/Jokes/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 class Jokes:
 	@staticmethod
-	def new(m,username,joke_title,joke_desc,joke_tags):
+	def new(m,username,joke_title,joke_desc,joke_tags,joke_date):
 		try:
-			m.db.jokes.save({"title": joke_title, "desc": joke_desc, "tags": joke_tags})
+			m.db.jokes.save({"title": joke_title, "desc": joke_desc, "tags": joke_tags, "date": joke_date})
 			return True
 		except:
 			return False
