e7f7806ac41be4d3913676a412af6a699b6d7369
Swaroop SM <swaroop.striker@gmail.com>
2012-10-27 16:59:39 +0530

    insert avatar in new function

--- src/wrappers/Jokes/__init__.py
+++ src/wrappers/Jokes/__init__.py
@@ -2,9 +2,9 @@
 
 class Jokes():
 	@staticmethod
-	def new(m,username,joke_title,joke_desc,joke_tags,joke_date):
+	def new(m,username,joke_title,joke_desc,joke_tags,joke_date,avatar):
 		try:
-			m.db.jokes.save({"username": username, "title": joke_title, "desc": joke_desc, "tags": joke_tags, "date": joke_date})
+			m.db.jokes.save({"username": username, "title": joke_title, "desc": joke_desc, "tags": joke_tags, "date": joke_date,"avatar": avatar})
 			return True
 		except:
 			return False
